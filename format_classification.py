84b6c4de047dd399ad8579716dc7b06766722cd1
yasath <31599563+yasath@users.noreply.github.com>
2019-01-02 11:17:31 +0000

    Added main Python file and filetype database

---
+++ format_classification.py
@@ -0,0 +1,69 @@
+file_formats = [
+    ["Archives", ["7z", "ace", "bz2", "dd", "deb", "gz", "gzip", "jar", "rar",
+                  "rpm", "sit", "sitx", "snb", "tar", "tar.gz", "tgz", "zip",
+                  "zipx"]],
+
+    ["Android Packages", ["apk"]],
+
+    ["Chrome Extensions", ["crx"]],
+
+    ["Project Files",
+        [
+         ["aup", "Audacity"],
+         ["rta", "TrueRTA"],
+         ["ise", "InstallShield Express"],
+         ["qxp", "QuarkXPress"],
+         ["mepx", "Movavi Video Editor"],
+         ["mswmm", "Windows Movie Maker"],
+         ["vep", "AVS Video Editor"],
+         ["veg", "Vegas Pro"],
+         ["vpj", "NCH VideoPad"],
+         ["wlmp", "Windows Live Movie Maker"]
+        ]
+     ],
+
+    ["Audio", ["3ga", "aa", "aax", "aif", "aifc", "aiff", "amr", "ape",
+               "au", "awb", "caf", "cda", "gsm", "iff", "kar", "koz",
+               "m4p", "m4r", "mid", "midi", "mmf", "mp2", "mpa",
+               "mpc", "mpga", "ogg", "oma", "opus", "qcp", "ra", "ram"]],
+
+    ["Music", ["aac", "flac", "m4a", "mp3", "wav", "wma"]],
+
+    ["Music",
+        [
+         ["asx", "Playlists"],
+         ["m3u", "Playlists"],
+         ["m3u8", "Playlists"],
+         ["xspf", "Playlists"],
+         ["wpl", "Playlists"]
+        ]
+     ],
+
+    ["CAD",
+        [
+         ["3dm", "McNeel Rhino"],
+         ["3ds", "Autodesk 3ds Max"],
+         ["dwg", "Autodesk AutoCAD"],
+         ["max", "Autodesk 3ds Max"],
+         ["wire", "Autodesk Alias"],
+         ["f3d", "Autodesk Fusion 360"],
+         ["ipt", "Autodesk Inventor"],
+         ["iam", "Autodesk Inventor"],
+         ["CATProduct", "CATIA"],
+         ["CATPart", "CATIA"],
+         ["x_b", "Parasolid"],
+         ["x_t", "Parasolid"],
+         ["sldprt", "Solidworks"],
+         ["sldasm", "Solidworks"],
+         ["slddrw", "Solidworks"],
+         ["skp", "SketchUp"],
+         ["123dx", "Autodesk 123D"]
+        ]
+     ],
+
+    ["CAD", ["dxf", "obj", "fbx", "ige", "iges", "igs", "prt", "g", "neu",
+             "sab", "sat", "smb", "smt", "stl", "ste", "step", "stp", "asm"]],
+
+    ["Data", ["adt", "clp"]]
+
+]
---
+++ script.py
@@ -0,0 +1,89 @@
+from os import path
+from os import rename
+from os import makedirs
+from os import sep as FOLDER_DELIMITER
+import glob
+from progress.bar import ChargingBar
+
+# Global Constants
+from format_classification import file_formats as FORMAT_CATEGORIES
+COPY_MODE = False  # Set to True to copy files instead of moving them
+
+
+def input_directory():
+    while True:
+        try:
+            directory = input("Please enter the directory to organise: ")
+            if not path.isdir(directory):
+                raise NotADirectoryError("Invalid directory entered",
+                                         directory)
+            break
+        except NotADirectoryError:
+            print("The entered directory is not valid. Try again.\n")
+    return(directory)
+
+
+def get_files(directory):
+    file_array = glob.glob(directory+FOLDER_DELIMITER+"*")
+
+    split_file_array = []
+    for file_path in file_array:
+        split_file_array.append(path.splitext(file_path))
+    return(split_file_array)
+
+
+def find_filetype(file_extension):
+    for category in FORMAT_CATEGORIES:
+        for subcategory in category[1]:
+            if type(subcategory) is list:
+                if file_extension == ("."+subcategory[0]):
+                    return([category[0], subcategory[1]])
+            else:
+                if file_extension == ("."+subcategory):
+                    return([category[0]])
+    return(["Miscellaneous"])
+
+
+def file_move(original_directory, file_path, file_category, new_filename):
+    original_path = file_path[0] + file_path[1]
+
+    new_path = original_directory
+    for folder in file_category:
+        new_path += (FOLDER_DELIMITER + folder)
+
+    try:
+        makedirs(new_path)
+    except FileExistsError:
+        pass
+
+    if new_filename:
+        new_path += (FOLDER_DELIMITER + new_filename)
+    else:
+        original_filename = path.split(original_path)[1]
+        new_path += (FOLDER_DELIMITER + original_filename)
+
+    rename(original_path, new_path)
+
+
+def main():
+    directory_to_organise = input_directory()
+    unorganised_files = get_files(directory_to_organise)
+
+    progress_bar = ChargingBar("Organising files...",
+                               max=len(unorganised_files))
+
+    for file_path in unorganised_files:
+        file_category = find_filetype(file_path[1])
+        # file_category = extra_categories(file_path, file_category)
+        # new_filename = file_rename(file_path)
+        new_filename = None
+        file_move(directory_to_organise, file_path,
+                  file_category, new_filename)
+        progress_bar.next()
+    progress_bar.finish()
+
+
+# MAIN PROGRAM
+
+if __name__ == "__main__":
+    main()
